719860179e3adc04dc1874d9307fd02abec917c5
guilhem <guilhem.marchand@gmail.com>
2019-04-01 23:30:18 +0100

    latest telegraf, init fix

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ PWD=`pwd`
 app=`basename $PWD`
 version=`grep 'version =' ${app}/local/app.conf | awk '{print $3}' | sed 's/\.//g'`
 
-tar -czf ${app}_${version}.tgz ${app} --exclude=${app}/metadata/local.meta
+tar -czf ${app}_${version}.tgz --exclude=${app}/metadata/local.meta ${app}
 echo "Wrote: ${app}_${version}.tgz"
 
 exit 0
